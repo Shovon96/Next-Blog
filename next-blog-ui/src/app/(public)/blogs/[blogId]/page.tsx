a632602ff34430dae9e9bd60d695c222c4a32c94
Shovon96 <fakhruddinshovon@gmail.com>
2025-09-29 00:52:54 +0600

    Fetch dynamic data using generateStaticParams() for SSG

--- next-blog-ui/src/app/(public)/blogs/[blogId]/page.tsx
+++ next-blog-ui/src/app/(public)/blogs/[blogId]/page.tsx
@@ -1,4 +1,14 @@
 import BlogDetailsCard from "@/components/modules/Blogs/BlogDetailsCard";
+import { IPost } from "@/types";
+
+
+export const generateStaticParams = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post`);
+    const {data: blogs} = await res.json()
+    return blogs?.slice(0, 2)?.map((blog: IPost) => ({
+        blogId: String(blog?.id)
+    }))
+}
 
 export default async function BlogDetailsPage({ params }: { params: Promise<{ blogId: string }> }) {
 
