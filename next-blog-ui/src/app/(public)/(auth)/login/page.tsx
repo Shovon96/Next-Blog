d4a781aab5dd11673176e7a35082c79534a04a60
Shovon96 <fakhruddinshovon@gmail.com>
2025-09-29 20:22:49 +0600

    login and registation page created with form

---
+++ next-blog-ui/src/app/(public)/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/modules/Auth/LoginForm";
+
+const LoginPage = () => {
+    return <div className="py-16 px-4 max-w-7xl mx-auto">
+        <LoginForm />;
+    </div>
+};
+
+export default LoginPage;
---
+++ next-blog-ui/src/app/(public)/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from "@/components/modules/Auth/RegisterForm";
+
+const RegisterPage = () => {
+    return <div className="py-16 px-4 max-w-7xl mx-auto">
+        <RegisterForm />;
+    </div>
+};
+
+export default RegisterPage;
