e3ecd7a2c62645d3f2278fbffb26b64fb9d27e4e
Shovon96 <fakhruddinshovon@gmail.com>
2025-09-30 23:53:51 +0600

    Implement NextAuth Credentials login system

--- next-blog-ui/src/helpers/authOptions.ts
+++ next-blog-ui/src/helpers/authOptions.ts
@@ -1,10 +1,57 @@
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { email } from "zod";
+
 
 export const authOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials) {
+
+                if (credentials?.email || credentials?.password) {
+                    return null
+                }
+
+                try {
+                    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/login`, {
+                        method: "POST",
+                        headers: {
+                            "Content-type": "application/json"
+                        },
+                        body: JSON.stringify({
+                            email: credentials?.email,
+                            password: credentials?.password
+                        })
+                    });
+                    if (!res.ok) {
+                        console.error("User Loging Failed", await res.text())
+                        return null
+                    }
+                    const user = await res.json();
+                    if (user.id) {
+                        return {
+                            id: user?.id,
+                            name: user?.name,
+                            email: user?.email,
+                            image: user?.picture
+                        }
+                    } else {
+                        return null
+                    }
+                } catch (error) {
+                    console.error("Error logging in:", error);
+                    return null
+                }
+            }
         })
     ],
     secret: process.env.AUTH_SECRET,
